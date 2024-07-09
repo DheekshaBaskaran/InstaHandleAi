fe34e5722a7a1b8f9a84d7b975c72a0e3c3894b6
Aadhith Rajinikanth <aadhithr18@gmail.com>
2024-07-09 00:59:45 -0700

    Initial commit

---
+++ imgDesc.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+headers = {"Authorization": "Bearer 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"}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+output = query("test.png")
+
+print(output)
